a8c8d27fb2a84e69187e9195068ed84037b760da
rawanmohameedd <nrawana12345@gmail.com>
2024-09-29 22:38:06 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT,()=>
+    console.log(`server is running on ${process.env.PORT}`))
