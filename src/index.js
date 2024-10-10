f9bf2c7a883fa8b390297e45a712da8384747c25
Rana <ranamohamad215@gmail.com>
2024-10-11 01:40:50 +0300

    Added the DB connection, a middleware that parses json and the authuntication routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const authRoutes = require("./routes/AuthRouts");
+const { default: mongoose } = require("mongoose");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.listen(process.env.PORT,()=>
-    console.log(`server is running on ${process.env.PORT}`))
+// middleware:
+app.use(express.json()); // parses an incomming JSON object into JavaScript
+
+// DB connection
+const dbURI =
+  "mongodb+srv://user1:mxRT34RF4543dmRF@cluster0.6y8ma.mongodb.net/";
+
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log("DB connected Successfully!");
+    // Positioned here to make sure the app doesn't start listenning before the db is connected
+    app.listen(process.env.PORT, () =>
+      console.log(`server is running on ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
+
+// Routes:
+app.use(authRoutes);
