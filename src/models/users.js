d60299a3de9c3dc59086f4262e1e1579be92b81a
Rana <ranamohamad215@gmail.com>
2024-10-10 21:27:01 +0300

    Created the user schema.

---
+++ src/models/users.js
@@ -0,0 +1,40 @@
+
+const mongoose = require("mongoose");
+
+// A user has an id, username, email, password, img url, status, contact list, group chats, createdAt, lastSeen
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  // imgURL: String,
+  status: String,
+  contactList: [{
+    type: mongoose.SchemaTypes.ObjectID,
+    ref: 'User'
+  }],
+  groupChats: [{
+    type: mongoose.SchemaTypes.ObjectID,
+    //? will be uncommented when the chat schema is built
+    // ref: 'Chat',
+  }],
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+  lastSeen: {
+    type: Date,
+    default: () => Date.now(),
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
