d5d720330b7ba49199b7346bb21c5974c0180d5a
Rana <ranamohamad215@gmail.com>
2024-10-11 22:37:13 +0300

    Added constrants on the email and password.

--- src/models/users.js
+++ src/models/users.js
@@ -1,4 +1,3 @@
-
 const mongoose = require("mongoose");
 
 // A user has an id, username, email, password, img url, status, contact list, group chats, createdAt, lastSeen
@@ -10,22 +9,32 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    lowercase: true,
   },
   password: {
     type: String,
     required: true,
+    minLength: 8,
   },
-  // imgURL: String,
+  profilePicture: {
+    type: String,
+    default: "",
+  },
   status: String,
-  contactList: [{
-    type: mongoose.SchemaTypes.ObjectID,
-    ref: 'User'
-  }],
-  groupChats: [{
-    type: mongoose.SchemaTypes.ObjectID,
-    //? will be uncommented when the chat schema is built
-    // ref: 'Chat',
-  }],
+  contactList: [
+    {
+      type: mongoose.SchemaTypes.ObjectID,
+      ref: "User",
+    },
+  ],
+  groupChats: [
+    {
+      type: mongoose.SchemaTypes.ObjectID,
+      //? will be uncommented when the chat schema is built
+      // ref: 'Chat',
+    },
+  ],
   createdAt: {
     type: Date,
     default: () => Date.now(),
