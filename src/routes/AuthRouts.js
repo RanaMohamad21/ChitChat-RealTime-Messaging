507631ecc9894a3319b6355938fe6d3ca831f7e3
Rana <ranamohamad215@gmail.com>
2024-10-13 19:20:44 +0300

    Added logout routes,

--- src/routes/AuthRouts.js
+++ src/routes/AuthRouts.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const router = Router();
 const authControllers = require("../controllers/authController");
-router.post("/signup", authControllers.signup_post);
-router.post("/login", authControllers.login_post);
+router.post("/signup", authControllers.signup);
+router.post("/login", authControllers.login);
+router.get("/logout", authControllers.logout);
 
 module.exports = router;
