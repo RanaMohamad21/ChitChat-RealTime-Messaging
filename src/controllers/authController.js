bee618d8bc4e9f6b27a09fbe0d9f4bdf8e028e63
Rana <ranamohamad215@gmail.com>
2024-10-13 19:19:25 +0300

    Finished the authentication controller.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,19 +1,76 @@
+require("dotenv").config();
 const User = require("../models/users");
-module.exports.signup_post = async (req, res) => {
+const jwt = require("jsonwebtoken");
+const maxAge = 3 * 24 * 60 * 60; // token max age in seconds.
+function errorHandler(err) {
+  let errors = { email: "", password: "", userName: "" };
+
+  // incorrect email
+  if (err.message === "incorrect email") {
+    errors.email = "that email is not registered.";
+  }
+  
+  // incorrect password
+  if (err.message === "incorrect password") {
+    errors.password = err.message;
+  }
+  // Duplicate email error code:
+  if (err.code === 11000) {
+    errors.email = "that email is already registered.";
+    return errors;
+  }
+
+  // Validation errors
+  if (err.message.includes("User validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+}
+
+// param: user id
+function createTocken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: maxAge });
+}
+
+module.exports.signup = async (req, res) => {
   const { email, userName, password } = req.body;
   try {
     const user = await User.create({ email, userName, password });
-    // Sending the user data back to the user and setting the status to success 
-    res.status(201).json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send('error, user not created');
+    const token = createTocken(user._id);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: maxAge * 1000, // max age in milli seconds
+    });
+    // Sending the user data back to the user and setting the status to success
+    res.status(201).json({ user: user._id }); // Sending only the user ID.
+  } catch (err) {
+    const errors = errorHandler(err);
+    res.status(400).json({ errors });
   }
-  res.send("Hello from sign up");
 };
 
-module.exports.login_post = async (req, res) => {
+module.exports.login = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
-  res.send("Hello from login");
+  try {
+    const user = await User.login(email, password);
+    const token = createTocken(user._id);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: maxAge * 1000, // max age in milli seconds
+    });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    console.log(err);
+    const errors = errorHandler(err);
+    res.status(400).json({errors});
+  }
 };
+
+module.exports.logout = (req,res)=>{
+  // remove the token
+  res.cookie('jwt', '', {maxAge: 1 });
+  res.redirect('/');
+}
