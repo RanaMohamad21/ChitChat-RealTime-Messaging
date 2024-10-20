90ab961f5b5e2c4523f1a8635a3cf7735c56bb37
Rana <ranamohamad215@gmail.com>
2024-10-20 16:09:03 +0300

    Added Cors.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ require("dotenv").config();
 const { app, server } = require("./socket/socket");
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/AuthRouts");
-const path = require("path")
-const fileUpload = require('express-fileupload');
-const MessageRoutes = require('./routes/MessagesRoutes')
-const userRoutes = require('./routes/userRoutes')
+const path = require("path");
+const fileUpload = require("express-fileupload");
+const MessageRoutes = require("./routes/MessagesRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://localhost:5173"], // frontend URL
+    credentials: true, // Allows cookies to be sent/received
+  })
+);
 
 // middleware:
 app.use(express.json()); // parses an incomming JSON object into JavaScript
-app.use(fileUpload()); 
+app.use(fileUpload());
 
 app.use(cookieParser());
 
@@ -27,9 +31,7 @@ server.listen(process.env.PORT, () =>
 
 connectDB();
 
-
 // Routes:
-app.use(authRoutes)
-app.use(MessageRoutes)
-app.use(userRoutes)
-
+app.use(authRoutes);
+app.use(MessageRoutes);
+app.use(userRoutes);
